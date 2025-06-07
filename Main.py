2bb50b29ed6bb4f57876496fe57cda0ad659da12
Quangv2yb <duyquangv2yb@gmail.com>
2025-06-07 16:11:00 +0700

    Lưu lại thay đổi trước khi pull

--- Main.py
+++ Main.py
@@ -4,12 +4,10 @@ from visualize.Printer import plot_vehicle_routes, plot_vehicle_routes_interacti
 
 if __name__ == "__main__":
     coord_dict, time_dict, demand_dict = print_one_txt_file("c203.txt")
-
-    # THAY ĐỔI: mỗi xe chọn 4 khách gần nhất, không trùng
     vehicle_routes = greedy_bfs_multi_vehicles(coord_dict, num_vehicles=25, max_customers_per_vehicle=4)
 
     for i, r in enumerate(vehicle_routes):
-        print(f"🚚 Xe {i + 1}: {r}")
+        print(f"Xe {i + 1}: {r}")
 
     plot_vehicle_routes(vehicle_routes, coord_dict, file_name="c203")
     plot_vehicle_routes_interactive(vehicle_routes, coord_dict, file_name="c203")
@@ -17,6 +15,6 @@ if __name__ == "__main__":
     qd_xe, qd_tong = tinh_quang_duong(vehicle_routes, coord_dict)
 
     for i, d in enumerate(qd_xe):
-        print(f"🚚 Quãng đường Xe {i+1}: {d:.2f}")
+        print(f"Quãng đường Xe {i+1}: {d:.2f}")
 
-    print(f"📦 Tổng quãng đường của tất cả 25 xe: {qd_tong:.2f}")
+    print(f"Tổng quãng đường của tất cả 25 xe: {qd_tong:.2f}")
